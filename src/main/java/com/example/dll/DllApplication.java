165088e7eff7fc8eba13598a341c5d9cf4321bcd
KSKamburova18 <KSKamburova18@codingburgas.bg>
2022-11-10 14:44:16 +0200

    Initial commit

---
+++ src/main/java/com/example/dll/DllApplication.java
@@ -0,0 +1,12 @@
+package com.example.dll;
+import java.sql.Date;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class DllApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DllApplication.class, args);
+    }
+
+}
